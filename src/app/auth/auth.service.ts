1774ddf7aa67d3e243d5ea8675af1bcbe5c07bf5
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-10-10 14:51:43 -0300

    reorganizando o service de login com a conta da google

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -54,27 +54,65 @@ export class AuthService {
     return this.afAuth.authState.pipe(map(u => (u) ? true : false))
   }
 
-  loginGoogle(): Observable<User> {
-    const provider = new auth.GoogleAuthProvider();
-    return from(this.afAuth.auth.signInWithPopup(provider))
-      .pipe(
-        tap((data) => console.log(data)),
-        switchMap((u: auth.UserCredential) => {
-          const newUser: User = {
-            firstname: u.user.displayName,
-            lastname: '',
-            address: '',
-            city: '',
-            state: '',
-            phone: '',
-            mobilephone: '',
-            email: u.user.email,
-            password: '',
-            id: u.user.uid
-          };
-          return this.userCollection.doc(u.user.uid)
-            .set(newUser).then(() => newUser);
-        })
-      );
+  async updateUserData(u: auth.UserCredential) {
+    try {
+      const newUser: User = {
+        firstname: u.user.displayName,
+        lastname: '',
+        address: '',
+        city: '',
+        state: '',
+        phone: '',
+        mobilephone: '',
+        email: u.user.email,
+        password: '',
+        id: u.user.uid
+      };
+      await this.userCollection.doc(u.user.uid).set(newUser);
+      return newUser;
+    }
+    catch(e) {
+      throw new Error(e);
+    }
   }
+
+  async loginWithGoogleAccount() {
+    try {
+      const provider = new auth.GoogleAuthProvider();
+      let credentials: auth.UserCredential = await this.afAuth.auth.signInWithPopup(provider);
+      let user: User = await this.updateUserData(credentials);
+      return user;
+    }
+    catch(e) {
+      throw new Error(e);
+    }
+  }
+
+  loginGoogle(): Observable<User> {
+    return from(this.loginWithGoogleAccount());
+  }
+
+  // loginGoogle(): Observable<User> {
+  //   const provider = new auth.GoogleAuthProvider();
+  //   return from(this.afAuth.auth.signInWithPopup(provider))
+  //     .pipe(
+  //       tap((data) => console.log(data)),
+  //       switchMap((u: auth.UserCredential) => {
+  //         const newUser: User = {
+  //           firstname: u.user.displayName,
+  //           lastname: '',
+  //           address: '',
+  //           city: '',
+  //           state: '',
+  //           phone: '',
+  //           mobilephone: '',
+  //           email: u.user.email,
+  //           password: '',
+  //           id: u.user.uid
+  //         };
+  //         return this.userCollection.doc(u.user.uid)
+  //           .set(newUser).then(() => newUser);
+  //       })
+  //     );
+  // }
 }
