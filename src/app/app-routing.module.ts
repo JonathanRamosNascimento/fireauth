91b512e936ca6881c66af79cf6ae7f83013a1bf0
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-10-10 14:07:07 -0300

    criando o authguard para proteger as rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuardService } from './auth/auth-guard.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/main/people'},
-  { path: 'main', loadChildren: './main/main.module#MainModule'},
+  { path: 'main', loadChildren: './main/main.module#MainModule', canActivate: [ AuthGuardService ]},
   { path: '**', component: NotFoundComponent}
 ];
 
